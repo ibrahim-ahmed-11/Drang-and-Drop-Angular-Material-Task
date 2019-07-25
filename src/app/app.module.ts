4b864d84712e26d393de852400439f8d8eb440b1
Ibrahim Ahmed <ibrahim-ahmed-11@users.noreply.github.com>
2019-07-25 17:52:16 +0200

    updated characters cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchComponent } from './search/search.component';
 import { MatInputModule } from '@angular/material/input';
