c4da48c0140965c54b3d47a8e19e056963601f65
Ibrahim Ahmed <ibrahim-ahmed-11@users.noreply.github.com>
2019-07-25 21:48:30 +0200

    added unit testing for HTTP service

--- src/app/services/get-from-api.service.spec.ts
+++ src/app/services/get-from-api.service.spec.ts
@@ -1,12 +1,56 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { GetFromAPIService } from './get-from-api.service';
 
 describe('GetFromAPIService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  
+  let service: GetFromAPIService;
+  let httpMock: HttpTestingController;
+  
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [GetFromAPIService]
+    });
+    service = TestBed.get(GetFromAPIService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
     const service: GetFromAPIService = TestBed.get(GetFromAPIService);
     expect(service).toBeTruthy();
   });
+
+  it('should get first character from the Star Wars API', () => {
+    service.getResults("people/1").subscribe((data: any) => {
+      expect(data.name).toBe('Luke Skywalker');
+    });
+
+    const req = httpMock.expectOne("https://swapi.co/api/people/1", 'Call API');
+    expect(req.request.method).toBe('GET');
+
+    req.flush({
+      name: 'Luke Skywalker'
+    });
+
+    httpMock.verify();
+
+  });
+
+  it('should get all the characters from the Star Wars API', () => {
+    service.getResults("people").subscribe((data: any) => {
+      expect(data.count).toBe(87);
+    });
+    
+    const req = httpMock.expectOne("https://swapi.co/api/people", 'Call API');
+    expect(req.request.method).toBe('GET');
+
+    req.flush({
+      count: 87
+    });
+
+    httpMock.verify();
+  });
+
 });
