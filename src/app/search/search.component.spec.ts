39e356ad3bdb914556df4f686c6c42a6c46d7114
Ibrahim Ahmed <ibrahim-ahmed-11@users.noreply.github.com>
2019-07-26 18:22:29 +0200

    fixed some uni testing errors

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,25 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { SearchComponent } from './search.component';
+import { GetFromAPIService } from './../services/get-from-api.service';
+import { DragulaService } from "ng2-dragula";
+import { IterableDiffers } from '@angular/core';
+import { MatExpansionPanelContent } from '@angular/material/expansion';
 
-describe('SearchComponent', () => {
-  let component: SearchComponent;
-  let fixture: ComponentFixture<SearchComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
-    })
-    .compileComponents();
-  }));
+describe('Search component unit test', () => {
+  let component : SearchComponent;
+  let dragula : DragulaService;
+  let httpMock: HttpTestingController;
+  let service: GetFromAPIService;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SearchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    dragula = new DragulaService();
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [GetFromAPIService]
+    });
+    service = TestBed.get(GetFromAPIService);
+    httpMock = TestBed.get(HttpTestingController);
+
+    component = new SearchComponent(dragula, service);
+
+    component.notFound = false;
+    component.isEmpty = false;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Try to get the result with no input data', () => {
+    component.getResult();
+    expect(component.isEmpty).toBe(true);
   });
+
 });
